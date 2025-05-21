1fe1baa112067e9696947d12dc37b08a14b1173a
Ryouya <Ryouyadesu0606@gmail.com>
2025-05-21 09:17:36 +0900

    リモートリポジトリの変更

---
+++ src/app/Http/Controllers/ContactController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function confirm(Request $request)
+    {
+        //ここに処理を記述していきます。
+        $contact = $request->only(['name', 'email', 'tel', 'contact']);
+        return view('confirm', compact('contact'));
+    }
+    public function store(Request $request)
+    {
+        $contact = $request->only(['name', 'email', 'tel', 'content']);
+        Contact::create($contact);
+        return view('thanks');
+    }
+}
+
